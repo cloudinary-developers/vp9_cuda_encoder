b3f6439d67be9bbbc94435ebc5b468a04b5c10bd
Eduard Sinelnikov <eduard@cloudinary.com>
2018-06-25 16:11:05 +0000

    Adding all

---
+++ build/vpxenc.c.d
@@ -0,0 +1,99 @@
+vpxenc.c.o vpxenc.c.d: ../vpxenc.c /usr/include/stdc-predef.h .././vpxenc.h \
+ .././vpx/vpx_encoder.h .././vpx/./vpx_codec.h .././vpx/././vpx_image.h \
+ .././vpx/././vpx_integer.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdint.h /usr/include/stdint.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/inttypes.h \
+ vpx_config.h /usr/include/assert.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdarg.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/xlocale.h /usr/include/x86_64-linux-gnu/bits/string.h \
+ /usr/include/x86_64-linux-gnu/bits/string2.h \
+ ../third_party/libyuv/include/libyuv/scale.h \
+ ../third_party/libyuv/include/libyuv/basic_types.h ../vpx/vpx_encoder.h \
+ ../vpx/vpx_decoder.h ../vpx/./vpx_codec.h ../vpx/./vpx_frame_buffer.h \
+ ../vpx/././vpx_integer.h .././args.h .././ivfenc.h ../././tools_common.h \
+ ../././vpx/vpx_codec.h ../././vpx/vpx_image.h ../././vpx/vpx_integer.h \
+ ../././vpx_ports/msvc.h .././././y4minput.h .././././vpx/vpx_image.h \
+ /usr/include/unistd.h /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ .././tools_common.h ../vpx/vp8cx.h ../vpx/./vp8.h ../vpx/././vpx_codec.h \
+ ../vpx/././vpx_image.h ../vpx/./vpx_encoder.h ../vpx/vp8dx.h \
+ ../vpx/vpx_integer.h ../vpx_ports/mem_ops.h \
+ ../vpx_ports/mem_ops_aligned.h ../vpx/vpx_integer.h vpx_config.h \
+ ../vpx_ports/vpx_timer.h /usr/include/x86_64-linux-gnu/sys/time.h \
+ .././rate_hist.h .././vpxstats.h .././warnings.h .././webmenc.h \
+ .././tools_common.h .././y4minput.h ../cuda/typedef_cuda.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/cuda.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/cuda_runtime.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/host_config.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/crt/host_config.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/builtin_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/device_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/host_defines.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/crt/host_defines.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/driver_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/vector_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/surface_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/texture_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/library_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/channel_descriptor.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/cuda_runtime_api.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/cuda_device_runtime_api.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/driver_functions.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/vector_functions.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/vector_functions.hpp \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/device_launch_parameters.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/cuda_texture_types.h \
+ /usr/local/cuda-9.1/targets/x86_64-linux/include/texture_fetch_functions.h \
+ ../vp8/common/mv.h ../cuda/init_cuda.h ../cuda/define_cuda.h \
+ ../vp8/common/onyxc_int.h vp8_rtcd.h \
+ ../vpx/internal/vpx_codec_internal.h ../vpx/internal/../vpx_decoder.h \
+ ../vpx/internal/../vpx_encoder.h /usr/include/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h ../vp8/common/loopfilter.h \
+ ../vpx_ports/mem.h ../vp8/common/entropymv.h ../vp8/common/treecoder.h \
+ ../vp8/common/entropy.h ../vp8/common/blockd.h ../vpx_scale/yv12config.h \
+ ../vpx/vpx_codec.h ../vpx/vpx_frame_buffer.h ../vp8/common/mv.h \
+ ../vp8/common/postproc.h ../vp8/common/onyxc_int.h \
+ ../vp8/common/ppflags.h ../vp8/common/header.h ../cuda/typedef_cuda.h \
+ ../cuda/frame_cuda.h ../cuda/me_cuda.h ../cuda/frame_cuda.h
